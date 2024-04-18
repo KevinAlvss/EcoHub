1750feb691fa0f9d4c01a1502f510e3fc992ef44
Estevam993 <joaovestevamr@gmail.com>
2024-04-17 22:30:59 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
+import { Index } from "./pages/index";
 import './styles/global.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/inicio" element={<Index />}/>
       </Routes>
     </BrowserRouter>
   );
