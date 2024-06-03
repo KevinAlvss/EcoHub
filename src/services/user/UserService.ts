b54cbd0a367ad2f8160b1fb775583a910301d16e
Mariana <mariana0026souza@gmail.com>
2024-06-02 21:24:32 -0300

    add project services

---
+++ src/services/user/UserService.ts
@@ -0,0 +1,35 @@
+import { AddOrUpdateUserRequest, GetUser } from "../models/user/UserModel";
+import { api } from "../axios";
+
+export class UserService {
+
+    async getAllUsers (): Promise<GetUser[]> {
+
+        const response = await api.get<GetUser[]>("/buscarUsuarios");
+        return response.data;
+    }
+
+    async getUserById (userId: string): Promise<GetUser> {
+
+        const response = await api.get<GetUser>(`api/v1/usuario/Usuario?userId=${userId}`)
+        return response.data;
+    }
+
+    async addNewUser (user: AddOrUpdateUserRequest): Promise<any> {
+
+        const response = await api.post("api/v1/usuario/Usuario",user);
+        return response.data;
+    }
+
+    async updateExistingUser (user: AddOrUpdateUserRequest, userId: string): Promise<any> {
+
+        const response = await api.put(`api/v1/usuario/Usuario?userId=${userId}`,user);
+        return response.data
+    }
+
+    async deleteExistingUser (userId: string): Promise<any> {
+
+        const response = await api.delete(`api/v1/usuario/Usuario?userId=${userId}`);
+        return response.data;
+    }
+ }
